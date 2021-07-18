96bb5a4be94ded2e4ca1bcd3d9c84b04c6dff784
Yass <YassineSalhi135@gmail.com>
2021-07-18 19:34:01 +0100

    created login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { login } from '../actions/userAction';
+import FormContainer from '../components/FormContainer';
+import { useSelector, useDispatch } from 'react-redux';
 
-const LoginPage = () => {
-  return <div></div>;
+const LoginPage = ({ history }) => {
+  const dispatch = useDispatch();
+  const { laoding, error, userInfo } = useSelector((state) => state.userLogin);
+  const [email, setEmail] = useState('');
+  const [redirect, setRedirect] = useState('');
+  const [password, setPassword] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    return () => {
+      if (userInfo) {
+        history.push();
+      }
+    };
+  }, []);
+
+  return (
+    <FormContainer>
+      <h1>Sign in</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button type='submit' className='my-3' variant='primary'>
+          Login
+        </Button>
+        <Row>
+          <Col>
+            New Customer?{' '}
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : `/register`}
+            >
+              Register
+            </Link>
+          </Col>
+        </Row>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default LoginPage;
